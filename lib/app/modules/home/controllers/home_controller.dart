f20591ab150d24365adaad514878cc7d4bb2ee1a
zihadsikder <zihadsikder.cse@gmail.com>
2024-12-19 21:22:01 +0600

    uploded image from camera also successs

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,22 +2,29 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomeController extends GetxController {
-
   RxString imagePath = ''.obs;
   final ImagePicker _picker = ImagePicker();
+
   @override
   void onInit() {
     super.onInit();
   }
 
-  Future<void> pickImage() async {
+  // Method to pick image from the gallery
+  Future<void> pickImageFromGallery() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
 
-    if (image!= null) {
+    if (image != null) {
       imagePath.value = image.path;
     }
   }
 
+  // Method to pick image using the camera
+  Future<void> pickImageFromCamera() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
 
-
+    if (image != null) {
+      imagePath.value = image.path;
+    }
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -22,13 +19,13 @@ class HomeView extends GetView<HomeController> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'select an image',
+              'Select a photo',
               style: TextStyle(fontSize: 20),
             ),
-            SizedBox(height: 48,),
             Obx(
-              () =>GestureDetector(
-                onTap: controller.pickImage,
+              () => GestureDetector(
+                onTap: controller.pickImageFromGallery,
+                // Default to gallery
                 child: Center(
                   child: Container(
                     height: 150,
@@ -36,22 +33,32 @@ class HomeView extends GetView<HomeController> {
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.white,
-                      border: Border.all(
-                        width: 15,
-                        color: Colors.grey.shade100
-                      )
+                      border: Border.all(width: 15, color: Colors.grey),
                     ),
-                    child : ClipOval(
-                        child: controller.imagePath.value.isEmpty ? Icon(Icons.person,size: 100)
-                            :
-                        Image.file(File(controller.imagePath.value),fit: BoxFit.cover,)
-                    )
-
-                   ),
+                    child: ClipOval(
+                      child: controller.imagePath.value.isEmpty
+                          ? Icon(Icons.person, size: 100)
+                          : Image.file(File(controller.imagePath.value)),
+                    ),
                   ),
                 ),
+              ),
             ),
-
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: controller.pickImageFromGallery,
+                  child: const Text('Pick from Gallery'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: controller.pickImageFromCamera,
+                  child: const Text('Take a Photo'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
